633907588de2aba252f92e35af830e1132223109
May Thu Nguyen <thumay@Thus-MacBook-Air.local>
2025-10-10 12:47:12 +1100

    update

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,14 +1,12 @@
 
-import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({
-  // post: postRouter,
-});
+export const appRouter = createTRPCRouter({});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
@@ -20,4 +18,6 @@ export type AppRouter = typeof appRouter;
  * const res = await trpc.post.all();
  *       ^? Post[]
  */
-export const createCaller = createCallerFactory(appRouter);
+export const createCallerFactory = (getCtx: () => Promise<Parameters<typeof appRouter.createCaller>[0]>) => {
+  return async () => appRouter.createCaller(await getCtx());
+};
